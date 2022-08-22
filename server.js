8940f66540a8222f73714011702144809c63365d
Lonardo17 <98488112+Lonardo17@users.noreply.github.com>
2022-08-22 12:46:47 +0300

    hw-3-ready

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
-const app = require('./app')
-
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const app = require('./app');
+const db = require('./models/contacts-db');
+const PORT = 3000;
+db.then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running. Use our API on port: 3000`);
+    });
+  }).catch(error => {
+    console.log(`Error: ${error.message}.`);
+  });
